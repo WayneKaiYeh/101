3ae1f96cf758261fdfc2f3c343dcf0fe96fb46db
Kai Yeh <a.jin820206@gmail.com>
2024-01-31 12:06:59 +0800

    310124/12:06

--- manufacturelog.py
+++ manufacturelog.py
@@ -26,44 +26,83 @@ def validate_and_extract_log(s):
     for batch in batches:
         dict = {}
 
-        #get the prefix
+        #Bet the Brefix
         prefix = batch[0]
 
-        #batch id
+        #Batch id
         batch_id = batch[1:5]
 
-        #product code
+        #Product code
         product_code = batch[6:8]
+        if not (product_code.isupper() and len(product_code) == 2 and product_code.isalpha()):
+            return "Invalid"
 
-        #quantity
-        q_index = batch.lower().find('q')
+        # Find the index of 'P'
+        p_index = batch.find('P')  
+
+        #Quantity
+        q_index = batch.lower().find('q',8)
         d_index = batch.lower().find('d', q_index)
         if q_index != -1 and d_index != -1:
             quantity = batch[q_index+1:d_index]
         else:
             quantity = None
 
-        #date
+        #Date
         if d_index != -1:
             date = batch[d_index+1:]
         else:
             date = None
 
 
-        #now we check the validation
-        if prefix != 'B':
-            # If not 'B', return "Invalid" and skip to the next batch
+        ### now we check the validation ###
+        if ' ' in s:
             return "Invalid"
-            #continue
+           
+        # If not 'B', return "Invalid"
+        # an invalid batch is detected, immediately exits the entire function and returns "Invalid" 
+        elif prefix != 'B':
 
-
-        elif (len(batch_id) != 4 and  batch_id.isdigit() != True):
+            return "Invalid"
+            
+        elif not (len(batch_id) == 4 and batch_id.isdigit() == True):
             return "Invalid"
 
+        elif p_index == -1 or batch[p_index] != 'P':
+            return "Invalid"
+
+        elif not (product_code.isupper() == True and len(product_code) == 2 and product_code.isalpha()== True):
+            return "Invalid"
         
-    return batches
+        elif not(quantity.isdigit()):
+            return "Invalid"
+        
+        # Validation for Quantity (check if 'Q' is uppercase)
+        elif batch[q_index] != 'Q':
+            return "Invalid"
 
 
-    #return batches_info or "invalid"
+        elif batch[d_index] != 'D':
+            return "Invalid"
 
 
+        elif not (date.isdigit() and len(date) == 8):
+            return "Invalid"
+        
+        elif (date.isdigit() and len(date) == 8): 
+            
+            year, month, day = int(date[:4]), int(date[4:6]), int(date[6:8])
+            if not (2000 <= year <= 2099) or not (1 <= month <= 12) or not (1 <= day <= 31):
+                return "Invalid"
+            
+            else:
+                #add the info
+                batches_info.append({
+                'Batch ID': batch_id,
+                'Product Code': product_code,
+                'Quantity': int(quantity),
+                'Date': date
+                })
+ 
+        
+    return batches_info
